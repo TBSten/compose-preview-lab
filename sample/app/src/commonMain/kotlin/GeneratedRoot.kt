9399468d089659da5830625172de2e72be409573
TBSten <81161390+TBSten@users.noreply.github.com>
2025-04-26 22:47:17 +0900

    不要なファイルを削除

--- sample/app/src/commonMain/kotlin/GeneratedRoot.kt
+++
@@ -1,9 +0,0 @@
-import androidx.compose.runtime.Composable
-
-annotation class GeneratedPreviewsList
-
-@GeneratedPreviewsList
-val samplePreviews = sequence<@Composable () -> Unit> {
-    // me.tbsten.compose.preview.lab.sample.App.kt
-    yield { me.tbsten.compose.preview.lab.sample.__Copied__App() }
-}
