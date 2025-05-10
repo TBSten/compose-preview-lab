4c440e08c5b33564ca19287a19f074ec203c690b
TBSten <81161390+TBSten@users.noreply.github.com>
2025-05-11 08:49:38 +0900

    previews.json を出力するように

--- composePreviewLabKspPlugin/build.gradle.kts
+++ composePreviewLabKspPlugin/build.gradle.kts
@@ -1,8 +1,11 @@
 plugins {
     alias(libs.plugins.jvm)
+    alias(libs.plugins.kotlinxSerialization)
 }
 
 dependencies {
     implementation(libs.kspApi)
     implementation(libs.kotlinCompilerEmbeddable)
+
+    implementation(libs.kotlinxSerializationJson)
 }
--- composePreviewLabKspPlugin/src/main/kotlin/me/tbsten/compose/preview/lab/ksp/plugin/GenerateList.kt
+++ composePreviewLabKspPlugin/src/main/kotlin/me/tbsten/compose/preview/lab/ksp/plugin/GenerateList.kt
@@ -2,8 +2,13 @@ package me.tbsten.compose.preview.lab.ksp.plugin
 
 import com.google.devtools.ksp.processing.CodeGenerator
 import com.google.devtools.ksp.processing.Dependencies
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.encodeToStream
 
 
+@OptIn(ExperimentalSerializationApi::class)
 internal fun generateList(
     previews: List<CopiedPreview>,
     codeGenerator: CodeGenerator,
@@ -31,4 +36,26 @@ internal fun generateList(
         it.appendLine(")")
         it.appendLine()
     }
+
+    codeGenerator.createNewFile(
+        dependencies = Dependencies(
+            aggregating = false,
+            sources = previews.map { it.baseFile }.toTypedArray(),
+        ),
+        packageName = "",
+        fileName = "previews",
+        extensionName = "json"
+    ).use { outputStream ->
+        Json.encodeToStream(
+            CollectedPreviewJsonData(
+                previewsListPackage = previewsListPackage,
+            ),
+            outputStream
+        )
+    }
 }
+
+@Serializable
+data class CollectedPreviewJsonData(
+    val previewsListPackage: String,
+)
