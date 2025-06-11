2d0cb19d38f77e859a650ab178873aa1aa2558d3
TBSten <81161390+TBSten@users.noreply.github.com>
2025-06-12 01:51:48 +0900

    `CollectedPreview` をインターフェースに変更

--- composePreviewLab/src/commonMain/kotlin/me/tbsten/compose/preview/lab/CollectedPreview.kt
+++ composePreviewLab/src/commonMain/kotlin/me/tbsten/compose/preview/lab/CollectedPreview.kt
@@ -2,9 +2,28 @@ package me.tbsten.compose.preview.lab
 
 import androidx.compose.runtime.Composable
 
-data class CollectedPreview(
-    val displayName: String,
-    val filePath: String,
-    val code: String? = null,
-    val content: @Composable () -> Unit,
+interface CollectedPreview {
+    val displayName: String
+    val filePath: String
+    val code: String?
+    val content: @Composable () -> Unit
+}
+
+internal data class CollectedPreviewImpl(
+    override val displayName: String,
+    override val filePath: String,
+    override val code: String? = null,
+    override val content: @Composable () -> Unit,
+) : CollectedPreview
+
+fun CollectedPreview(
+    displayName: String,
+    filePath: String,
+    code: String? = null,
+    content: @Composable () -> Unit,
+): CollectedPreview = CollectedPreviewImpl(
+    displayName = displayName,
+    filePath = filePath,
+    code = code,
+    content = content,
 )
