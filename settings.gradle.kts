56f4a2e254d9cba952b07577aca3186181d34ef3
TBSten <81161390+TBSten@users.noreply.github.com>
2025-06-22 06:40:18 +0900

    gradle plugin モジュールを追加

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -32,7 +32,7 @@ dependencyResolutionManagement {
     }
 }
 plugins {
-    //https://github.com/JetBrains/compose-hot-reload?tab=readme-ov-file#set-up-automatic-provisioning-of-the-jetbrains-runtime-jbr-via-gradle
+    // https://github.com/JetBrains/compose-hot-reload?tab=readme-ov-file#set-up-automatic-provisioning-of-the-jetbrains-runtime-jbr-via-gradle
     id("org.gradle.toolchains.foojay-resolver-convention").version("0.10.0")
 }
 
@@ -40,4 +40,5 @@ includeBuild("./buildLogic")
 include(":composePreviewLab")
 include(":composePreviewLabAnnotation")
 include(":composePreviewLabKspPlugin")
+include("composePreviewLabGradlePlugin")
 include("composePreviewLabIntellijPlugin")
