188d264dcf04b528d79705d931d24f9e9760d169
TBSten <81161390+TBSten@users.noreply.github.com>
2025-07-05 08:19:22 +0900

    featured files を デバッグ時の main 関数に追加

--- core/src/jvmDev/kotlin/Main.kt
+++ core/src/jvmDev/kotlin/Main.kt
@@ -1,8 +1,10 @@
+import me.tbsten.compose.preview.lab.featuredFilesForDebug
 import me.tbsten.compose.preview.lab.openfilehandler.OpenFileHandler
 import me.tbsten.compose.preview.lab.previewLabApplication
 import me.tbsten.compose.preview.lab.previewsForUiDebug
 
 fun main(): Unit = previewLabApplication(
     previews = previewsForUiDebug,
+    featuredFiles = featuredFilesForDebug,
     openFileHandler = OpenFileHandler { TODO("open file: $it") },
 )
