1cc76d5e8ac43df81d2a3fd94750a8a8f0a35e8e
TBSten <programmingCafeteria@gmail.com>
2025-09-05 19:23:27 +0900

    WithHintField を改修

--- core/src/commonMain/kotlin/me/tbsten/compose/preview/lab/field/WithHintField.kt
+++ core/src/commonMain/kotlin/me/tbsten/compose/preview/lab/field/WithHintField.kt
@@ -2,16 +2,13 @@ package me.tbsten.compose.preview.lab.field
 
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import me.tbsten.compose.preview.lab.component.Divider
 import me.tbsten.compose.preview.lab.ui.PreviewLabTheme
 import me.tbsten.compose.preview.lab.ui.components.Chip
 import me.tbsten.compose.preview.lab.ui.components.Text
@@ -24,6 +21,9 @@ import me.tbsten.compose.preview.lab.ui.components.Text
  */
 fun <Value> MutablePreviewLabField<Value>.withHint(vararg choices: Pair<String, Value>): MutablePreviewLabField<Value> =
     WithHintField<Value>(this, choices = mapOf(*choices))
+
+fun <Value> MutablePreviewLabField<Value>.withInitialValueHint(): MutablePreviewLabField<Value> =
+    withHint("Reset" to this.initialValue)
 
 /**
  * A field wrapper that adds hint choices below the base field.
@@ -43,13 +43,10 @@ class WithHintField<Value> internal constructor(
 ) {
     @Composable
     override fun Content() {
-        Column {
-            Box(
-                modifier = Modifier
-                    .padding(start = 16.dp, end = 6.dp),
-            ) { super.Content() }
-
-            Divider(modifier = Modifier.padding(vertical = 4.dp))
+        Column(
+            verticalArrangement = Arrangement.spacedBy(4.dp),
+        ) {
+            super.Content()
 
             Row(
                 horizontalArrangement = Arrangement.spacedBy(8.dp),
